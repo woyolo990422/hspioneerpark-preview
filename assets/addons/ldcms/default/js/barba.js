02f2192be1098deb8b0089b154e5592dec480e7e
woyolo990422 <woyolo990422@users.noreply.github.com>
2026-06-27 16:09:56 +0800

    Fix static preview links

--- assets/addons/ldcms/default/js/barba.js
+++ assets/addons/ldcms/default/js/barba.js
@@ -44,7 +44,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.c = installedModules;
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://localhost:8080/dist";
+/******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
